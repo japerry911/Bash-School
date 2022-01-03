2f6b2b1c90ce4c1e964820e820100ac3cfc9ee1f
Jack Perry <jack.e.perry@outlook.com>
2022-01-03 07:09:46 -0700

    👌 IMPROVE: exercises added

--- exercises/error_handling.sh
+++ exercises/error_handling.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+main() {
+  echo $#
+
+  if [ "$#" -ne 1 ]; then
+    echo "Usage: error_handling.sh <person>"
+    exit 1
+  else
+    echo "Hello, ${1}"
+    exit 0
+  fi
+}
+
+main "$@"
