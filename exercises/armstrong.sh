3706b59f8c103fd727b2bb854ddec6db818df662
Jack Perry <jack.e.perry@outlook.com>
2022-01-03 17:54:08 -0700

    👌 IMPROVE: progress

--- exercises/armstrong.sh
+++ exercises/armstrong.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 main() {
-    if $# -ne 1; then
+    if [ $# -ne 1 ]; then
         echo "usage: armstrong.sh <integer1>"
         exit 1
     fi
+
+    declare -i NUMBER=$1
+    declare -i NUMBER_LENGTH="${#NUMBER}"
+    echo $NUMBER_LENGTH
 }
 
-main "?@"
+main 153
