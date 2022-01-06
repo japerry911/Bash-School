4f85fab4ece7d208e9fe733e4ed8096888c8dc88
Jack Perry <jack.e.perry@outlook.com>
2022-01-06 04:55:48 -0700

    👌 IMPROVE: progress/address exercise started

--- exercises/luhn_algorithm.sh
+++ exercises/luhn_algorithm.sh
@@ -5,7 +5,15 @@ main() {
     if [[ $NUMBER_TO_VALIDATE -le 1 ]]; then
         echo "false"
     fi
+
+    declare -i LENGTH=${#NUMBER_TO_VALIDATE}
+    declare -i TOTAL=0
+
+    echo $LENGTH
 }
 
-main "1"
-# false
+# main "1"
+# # false
+
+main "059"
+# true
